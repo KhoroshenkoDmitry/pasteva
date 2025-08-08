25471477fa3975493ea8be467d3840ffb9db2370
Asteri <khoroshenkodmitry@gmail.com>
2025-08-08 13:01:36 +0300

    init spring commit

---
+++ src/main/java/dev/asteri/pasteva/PastevaApplication.java
@@ -0,0 +1,13 @@
+package dev.asteri.pasteva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastevaApplication.class, args);
+	}
+
+}
